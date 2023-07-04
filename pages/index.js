741d541f028772045c9f060be6dab1e643c8fd2f
backcost <gabriel_costa_ferreira@hotmail.com>
2023-07-03 22:43:03 -0300

    add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-	return <h1>Julha, eu amo vc. Se vc me ama, dá uma risadinha</h1>
+  return <h1>Julha, eu amo vc.da Se vcdad me ama, d uma risadinha</h1>;
 }
 
 export default Home;
